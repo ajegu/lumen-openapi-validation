559452df4a1e64e42cc807bbe990d30728e5d557
Allan JEGU <allan.jegu@treezor.com>
2022-10-15 10:19:59 +0200

    POC OpenAPI PS7 Validation

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 class ExampleController extends Controller
 {
     /**
@@ -14,5 +16,13 @@ class ExampleController extends Controller
         //
     }
 
-    //
+    public function index(): JsonResponse
+    {
+        return new JsonResponse([
+            [
+                'id' => 123,
+                'name' => 'foo'
+            ]
+        ]);
+    }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,11 +2,21 @@
 
 namespace Tests;
 
+use GuzzleHttp\Psr7\HttpFactory;
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+use League\OpenAPIValidation\PSR7\OperationAddress;
+use League\OpenAPIValidation\PSR7\ValidatorBuilder;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 
 class ExampleTest extends TestCase
 {
+    public function setUp(): void {
+        parent::setUp();
+//        config(['app.url' => 'http://127.0.0.1/dashboard']);
+
+    }
+
     /**
      * A basic test example.
      *
@@ -14,10 +24,27 @@ class ExampleTest extends TestCase
      */
     public function test_that_base_endpoint_returns_a_successful_response()
     {
-        $this->get('/');
+
+        $response = $this->call('GET', '/pets?foo=bar');
+
+        $psr17Factory = new HttpFactory();
+        $psrHttpFactory = new PsrHttpFactory($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
+        $psrRequest = $psrHttpFactory->createRequest($this->app['request']);
+
+        $yamlPath = __DIR__ . '/../petstore.yml';
+        $validatorBuilder = (new ValidatorBuilder())
+            ->fromYamlFile($yamlPath);
+        $requestValidator = $validatorBuilder->getServerRequestValidator();
+        $responseValidator = $validatorBuilder->getResponseValidator();
+
+        $op = $requestValidator->validate($psrRequest);
+
+        $psr7Response = $psrHttpFactory->createResponse($response->baseResponse);
+        $responseValidator->validate($op, $psr7Response);
 
         $this->assertEquals(
-            $this->app->version(), $this->response->getContent()
+            '[{"id":123,"name":"foo"}]',
+            $this->response->getContent()
         );
     }
 }
